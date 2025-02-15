7b45028155bed0c54878dc259256bf7ce7a6baee
Francisco Angelo <franciscoangelo.feo@gmail.com>
2025-02-15 19:31:37 -0300

    first commit

---
+++ app/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+var db *sql.DB
+
+type Record struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func initDB() {
+	var err error
+	connStr := "user=postgres dbname=cruddb password=postgres sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS records (
+			id SERIAL PRIMARY KEY,
+			name TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		);
+	`)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addRecord(w http.ResponseWriter, r *http.Request) {
+	var record Record
+	if err := json.NewDecoder(r.Body).Decode(&record); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	query := `INSERT INTO records (name) VALUES ($1) RETURNING id, created_at`
+	err := db.QueryRow(query, record.Name).Scan(&record.ID, &record.CreatedAt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(record)
+}
+
+func getAllRecords(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("SELECT id, name, created_at FROM records")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var records []Record
+	for rows.Next() {
+		var record Record
+		if err := rows.Scan(&record.ID, &record.Name, &record.CreatedAt); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		records = append(records, record)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(records)
+}
+
+func deleteRecord(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		http.Error(w, "Missing id parameter", http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.Exec("DELETE FROM records WHERE id = $1", id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func main() {
+	initDB()
+
+	http.HandleFunc("/add", addRecord)
+	http.HandleFunc("/records", getAllRecords)
+	http.HandleFunc("/delete", deleteRecord)
+
+	fmt.Println("Server is running on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ infra/infra.dockerfile
@@ -0,0 +1,2 @@
+FROM newrelic/infrastructure:latest
+ADD ./infra/newrelic-infra.yml /etc/newrelic-infra.yml
